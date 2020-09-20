4aa746f5c82bad48a56e6fcaf6653d906ea703f3
surabhi013 <surabhi.ag18@gmail.com>
2020-09-20 18:33:20 +0530

    Fix container spacing

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -3,10 +3,12 @@ import OHLCChart from './OHLCChart';
 const Overview = ({data}) => {
     return(
         <React.Fragment>
-            <div className='container'><h3>Historical Data</h3></div>
-            {data &&
-                <OHLCChart data={data} />
-            }
+            <div className='container'>
+                <h3>Historical Data</h3>
+                {data &&
+                    <OHLCChart data={data} />
+                }
+            </div>
         </React.Fragment>
     )
 };
